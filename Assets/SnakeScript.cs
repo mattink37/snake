69d88271c54db1df0ed930caa2052be225f6bdd2
Matt Inkeles <mattinkeles37@gmail.com>
2023-08-04 18:07:03 -0400

    snake monch + segment movement

--- Assets/SnakeScript.cs
+++ Assets/SnakeScript.cs
@@ -11,14 +11,15 @@ public class SnakeScript : MonoBehaviour
     private Rigidbody2D rb = null;
     private SpriteRenderer spriteRenderer = null;
     private Coroutine gameStart = null;
+    private Boolean pendingSegment = false;
 
     public float moveInterval = 0.5f;
 
     private void Awake()
     {
         input = new SnakeControls();
-        rb = GetComponentInChildren<Rigidbody2D>();
-        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -37,7 +38,22 @@ public class SnakeScript : MonoBehaviour
     {
         while (true)
         {
-            rb.position += moveVector * spriteRenderer.bounds.size.x;
+            var snakeyBits = GameObject.FindGameObjectsWithTag("SnakeSegment");
+            Transform previousParentTransform = null;
+            foreach (GameObject snakeyBit in snakeyBits)
+            {
+                if (previousParentTransform == null) {
+                    previousParentTransform = snakeyBit.transform;
+                    snakeyBit.GetComponent<Rigidbody2D>().position += moveVector * spriteRenderer.bounds.size.x;
+                } else {
+                    snakeyBit.transform.position = previousParentTransform.position;
+                    previousParentTransform = snakeyBit.transform;
+                }
+            }
+            if (pendingSegment) {
+                Grow();
+                pendingSegment = false;
+            }
             yield return new WaitForSeconds(moveInterval);
         }
     }
@@ -54,4 +70,26 @@ public class SnakeScript : MonoBehaviour
     {
         moveVector = value.ReadValue<Vector2>();
     }
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        Debug.Log("Collision");
+        if (collider2D.CompareTag("Food"))
+        {
+            Destroy(GameObject.FindWithTag("Food"));
+            pendingSegment = true;
+        }
+        else if (collider2D.CompareTag("SnakeSegment"))
+        {
+            Debug.Log("Collided with snake!");
+        }
+    }
+
+    private void Grow() 
+    {
+        GameObject caboose = Instantiate(Resources.Load<GameObject>("SnakeBody"));
+        caboose.transform.SetParent(this.transform, false);
+        caboose.transform.SetSiblingIndex(0);
+    }
+
 }
